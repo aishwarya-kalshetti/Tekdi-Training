5b1f8572531293b8d2f205579d50fa63743f3d88
aishwarya-kalshetti <aishwarya.kalshetti@tekditechnologies.com>
2025-08-16 00:51:38 +0530

    Add files via upload

---
+++ Click to Change Background Color/ass1.js
@@ -0,0 +1,6 @@
+const btn=document.getElementById("Btn");
+btn.addEventListener("click",function(){
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+
+});
