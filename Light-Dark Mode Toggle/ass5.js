42c46fc5ab891816619bdff5c22b7901f4798c82
aishwarya-kalshetti <aishwarya.kalshetti@tekditechnologies.com>
2025-08-16 15:12:10 +0530

    Add files via upload

---
+++ Light-Dark Mode Toggle/ass5.js
@@ -0,0 +1,13 @@
+let mode=document.querySelector("#mode");
+let currentMode="light";
+mode.addEventListener("click",()=>{
+if(currentMode==="light"){
+    currentMode="dark";
+    document.querySelector("body").style.backgroundColor="black";
+}
+else{
+    currentMode="light";
+    document.querySelector("body").style.backgroundColor="white";
+}
+console.log(currentMode);
+});
